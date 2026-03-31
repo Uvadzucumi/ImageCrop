a3aab0b6819ede075b0cbe73b833de6c16cafc62
Uvadzucumi <uvadzucumi@gmail.com>
2026-03-31 21:47:10 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Processing;
+
+namespace ImageCrop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length > 0)
+            {
+                String sourceFileName = args[0];
+                Console.WriteLine($"image_file: {sourceFileName}");
+
+                try
+                {
+                    using (Image image = Image.Load(sourceFileName))
+                    {
+
+                        // check width & height
+                        int w = image.Width;
+                        int h = image.Height;
+
+                        if (w == 4096 && h == 2048)
+                        {
+                            int halfWidth = w / 2;
+                            int height = 2048;
+
+                            var encoder = new PngEncoder
+                            {
+                                CompressionLevel = PngCompressionLevel.Level6
+                            };
+
+                            string nameOnly = Path.GetFileNameWithoutExtension(sourceFileName);
+
+                            // left side
+                            using (Image leftPart = image.Clone(ctx => ctx.Crop(new Rectangle(0, 0, halfWidth, height))))
+                            {
+                                leftPart.Save(nameOnly + "_1001.png", encoder);
+                            }
+
+                            // right side
+                            using (Image rightPart = image.Clone(ctx => ctx.Crop(new Rectangle(halfWidth, 0, halfWidth, height))))
+                            {
+                                rightPart.Save(nameOnly + "_1002.png", encoder);
+                            }
+
+                            Console.WriteLine("Done! Two 2048x2048 images created.");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: Found image: " + w + "x" + h + ". required 4096x2048 size!");
+                        }
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Required image file name");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# ImageCrop
-crop png texture file with size 4096 x 2048 to 2 textures 2048 x 2048 (required for UE virtual textures)
+# TextureCrop
+crop png texture file with size 4096 x 2048 to 2 textures 2048 x 2048 and add _1001, _1002 for new file names. 
+required for import to Unrel Engine aka Virtual Texture 
+
+## usage example
+```ImageCrop.exe file_name.png```
+crete 2 fileы file_name_1001.png & file_name_1002.png for import to Unreal Engine
+
+## add requirements
+```
+dotnet add package SixLabors.ImageSharp
+```
+## build with all dependences. including all .net requirements
+~16 Mb.
+```
+dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeNativeLibrariesForSelfExtract=true
+```
+## buid without .NET, include only ImageSharp
+~2 Mb.
+```
+dotnet publish -c Release -r win-x64 --self-contained false -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true
+```
